35d4334afc7b72aaa336ba23269fd57d263e5fe6
Matheus Santana <santalvesmatheus@gmail.com>
2024-01-15 20:57:56 -0300

    Atualizar o tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  safelist: ["text-white"],
   theme: {},
   plugins: [require("daisyui")],
   daisyui: {
