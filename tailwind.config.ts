9d61d6b232ebda5c46bb46665298f2668f6e6935
Matheus <mat.alvessantana@gmail.com>
2024-01-15 17:50:46 -0300

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,34 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  theme: {},
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+          "primary": "#FFA7C9",
+          "secondary": "#FF6BA4",
+          "white": "#EBF4EE",
+          "primary-content": "#FFD7E6",
+          "neutral": "#FF6BA4",
+        },
+        dark: {
+          "primary": "#FFA7C9",
+          "secondary": "#FF6BA4",
+          "white": "#EBF4EE",
+          "primary-content": "#FFD7E6"
+        },
       },
-    },
-  },
-  plugins: [],
+    ],
+    darkTheme: "dark",
+    base: true,
+    styled: true,
+    utils: true,
+    rtl: false,
+    prefix: "",
+    logs: true,
+  }
 }
 export default config
