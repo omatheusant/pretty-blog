4b24561880a88be73d45f744ff31aafe403eb80d
Matheus <mat.alvessantana@gmail.com>
2024-01-31 16:41:40 -0300

    update donate page

--- app/(dashboard)/donate/page.tsx
+++ app/(dashboard)/donate/page.tsx
@@ -1,8 +1,13 @@
+import Image from "next/image"
+import {Heart} from 'lucide-react'
+
 const DonatePage = () => {
   return (
-    <div className="flex flex-col">
-      <h1>Contribua com meus projetos!{' <3'}</h1>
-      <h2>Chave PIX: 16992265097</h2>
+    <div className="flex flex-col items-center mt-20  text-center">
+      <h1 className="text-4xl sm:text-6xl flex">Gostaria de me ajudar? <Heart size={30} /></h1>
+      <h2 className="text-3xl sm:text-4xl">Chave PIX: 16992265097</h2>
+
+      <Image className="" src={'/cute.gif'} width={200} height={200} alt=""/>
     </div>
   )
 }
